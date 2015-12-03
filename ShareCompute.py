7c042602b6b25272bd0a02a4196828cef759ced0
C.L <orcuslc@hotmail.com>
2015-12-03 18:22:26 +0800

    Update ShareCompute.py

--- ShareCompute.py
+++ ShareCompute.py
@@ -3,10 +3,10 @@
 
 def Profit(shares):
 	if shares:
-		count = 0
 		profit = 0
+		asset = 0
 		for share in shares:
+			asset += share['close'] * share['holdingQuantity']
 			profit += share['holdingQuantity'] * share['price_change']
-			count += 1
-		return "Profit rate yesterday is :{0}".format(profit / count)
+		return "Profit rate yesterday is :{0}".format(profit / asset)
 	else: return "Please choose shares ASAP."
