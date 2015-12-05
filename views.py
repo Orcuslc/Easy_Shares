f031ff5a399ad51c547d536b258a6cea3a70e93f
chuanlu <orcuslc@hotmail.com>
2015-12-06 00:58:19 +0800

    completely changed  views

--- views.py
+++ views.py
@@ -1,24 +1,68 @@
-#!/usr/bin/env python
+﻿"""
+Routes and views for the flask application.
+"""
 
-# all the imports
-from ShareCompute import *
+from datetime import datetime
+from __init__ import app
 import sqlite3
-from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
-import datetime
+from ShareCompute import Profit
 
-# configuration
-DATABASE = '/var/www/Stock/Stock/DataBase/stock.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        today = datetime.now().date(),
+        title='Home Page',
+        year=datetime.now().year,
+    )
 
-#create application 
-app = Flask(__name__)
-app.config.from_object(__name__)
-#app.config.from_envvar('TESTBLOG_SETTINGS',silent = True)
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Please contact us if you have any questions.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Powered by Flask, Pandas, Tushare, Sqlalchemy'
+    )
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You are logged in')
+            return redirect(url_for('home'))
+    return render_template(
+        'login.html',
+        title = 'Login',
+        year = datetime.now().year,
+        error = error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You are logged out')
+    return redirect(url_for('home'))
 
 #function to connect database
 def connect_db():
@@ -42,16 +86,16 @@ def teardown_request(exception):
 	g.db.close()
 
 #show shares
-@app.route('/')
+@app.route('/show_shares')
 def show_shares():
-	today = str(datetime.date.today())
+	today = datetime.now().date
 	cur = g.db.execute('SELECT DISTINCT X.code, name,\
 							   date, holdingQuantity, close, price_change\
 						FROM (SELECT code, name, holdingQuantity\
 						 	  FROM stock_basics\
 						 	  WHERE holdingQuantity > 0) AS X,\
 							 (SELECT code, date, close, price_change\
-							  FROM  (SELECT code, date, close, price_change FROM stock_price) AS Y\
+                        	  FROM  (SELECT code, date, close, price_change FROM stock_price) AS Y\
 							  WHERE date in (SELECT MAX(date) \
 							  				 FROM (SELECT code, date FROM stock_price) AS Z\
 							  				 WHERE Z.code = Y.code)) AS W\
@@ -62,7 +106,7 @@ def show_shares():
 	return render_template('show_shares.html', shares = shares, profit = Profit(shares), date = today)
 
 #search_share
-@app.route('/search', methods=['GET', 'POST'])
+@app.route('/search_shares', methods=['GET', 'POST'])
 def search_shares():
 	if not request.form['code']:
 		return redirect(url_for('show_shares'))
@@ -81,7 +125,11 @@ def search_shares():
 						ORDER BY X.code ASC', [request.form['code'] for i in range(4)])
 	shares = [dict(code=row[0], name=row[1], c_name=row[2], holdingQuantity=row[3],\
 				   close=row[4], price_change=row[5], date=row[6]) for row in cur.fetchall()]
-	return render_template('search_shares.html', shares = shares)
+	return render_template(
+        'search_shares.html', 
+        title = 'Search',
+        shares = shares,
+        year = datetime.now().year)
 
 #modify_share
 @app.route('/modify/<code>', methods=['POST'])
@@ -104,29 +152,5 @@ def del_share():
 	g.db.execute('DELETE FROM ENTRIES WHERE ID = ?', [request.form['code']])
 	g.db.commit()
 	flash('Selected entry was successfully delete')
-	return redirect(url_for('show_shares'))
+	return redirect(url_for('show_shares'))           
 
-#login and logout
-@app.route('/login',methods=['GET', 'POST'])
-def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != app.config['USERNAME']:
-			error = 'Invalid username'
-		elif request.form['password'] != app.config['PASSWORD']:
-			error = 'Invalid password'
-		else:
-			session['logged_in'] = True
-			flash('You were logged in')
-			return redirect(url_for('show_shares'))
-	return render_template('login.html', error=error)
-
-@app.route('/logout')
-def logout():
-	session.pop('logged_in', None)
-	flash('You were logged out')
-	return redirect(url_for('show_shares'))
-
-
-if __name__ == '__main__':
-	app.run(host = '0.0.0.0')
