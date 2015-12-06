30eba06e7611a3138acb9a10e6e1f86087ab764e
chuanlu <orcuslc@hotmail.com>
2015-12-07 00:31:51 +0800

    do some changes in handling Errors in initing database

--- init_database.py
+++ init_database.py
@@ -6,10 +6,11 @@ import tushare as ts
 import sqlite3 as sql3
 import sqlalchemy as sqlalc
 import pandas as pd 
+import datetime as dt
 
-route = 'sqlite:///F:\\Documents\\GitHub\\StockQuerySystem\\Stock\\DataBase\\stock.db'
+route = 'sqlite:///F:\\Documents\\GitHub\\Stock\\DataBase\\stock.db'
 engine = sqlalc.create_engine(route)
-
+today = str(dt.date.today())
 
 stock_basics = pd.read_sql('SELECT * from stock_basics', engine)
 if stock_basics.empty == True:
@@ -18,14 +19,19 @@ if stock_basics.empty == True:
 	stock_basics.to_sql('stock_basics', engine, if_exists = 'replace')
 	stock_concept.to_sql('stock_concept', engine, if_exists = 'replace')
 
+exist_list = ['00000' + str(i) for i in range(10)]+['0000' + str(i) for i in range(10, 23)]
 for stock in stock_basics['code']:
 	try:
-		stock_price = ts.get_hist_data(stock)
-		stock_price.loc[:, 'code'] = stock
-		index = stock_price.index
-		stock_price = stock_price.set_index(['code', index])
-		print(stock_price.index)
-		stock_price.to_sql('stock_price', engine, if_exists = 'append')
+		if stock not in exist_list:
+			stock_price = ts.get_hist_data(stock, start='2015-11-28', end=today)
+			stock_price.loc[:, 'code'] = stock
+			index = stock_price.index
+			stock_price = stock_price.set_index(['code', index])
+			print(stock_price.index)
+			stock_price.to_sql('stock_price', engine, if_exists = 'append')
 	except AttributeError:
 		print("AttributeError", stock)
+		continue
+	except ValueError:
+		print("ValueError", stock)
 		continue
