312e74757e5a6bdd032b9fb8aea25d20db38f819
chuanlu <orcuslc@hotmail.com>
2015-12-08 15:19:51 +0800

    did some modifying work to init the database automaticlly everyday, with a sleep for 72000 seconds

--- init_database.py
+++ init_database.py
@@ -7,31 +7,38 @@ import sqlite3 as sql3
 import sqlalchemy as sqlalc
 import pandas as pd 
 import datetime as dt
+import threading
+import time
 
-route = 'sqlite:///F:\\Documents\\GitHub\\Stock\\DataBase\\stock.db'
-engine = sqlalc.create_engine(route)
-today = str(dt.date.today())
+hour = dt.datetime.now().hour
 
-stock_basics = pd.read_sql('SELECT * from stock_basics', engine)
-if stock_basics.empty == True:
-	stock_basics = ts.get_stock_basics()
-	stock_concept = ts.get_concept_classified()
-	stock_basics.to_sql('stock_basics', engine, if_exists = 'replace')
-	stock_concept.to_sql('stock_concept', engine, if_exists = 'replace')
+while True:
+	if hour == 20:
+		route = 'sqlite:////var/www/Stock/Stock/DataBase/stock.db'
+		engine = sqlalc.create_engine(route)
+		today = str(dt.date.today())
 
-exist_list = ['00000' + str(i) for i in range(10)]+['0000' + str(i) for i in range(10, 23)]
-for stock in stock_basics['code']:
-	try:
-		if stock not in exist_list:
-			stock_price = ts.get_hist_data(stock, start='2015-11-28', end=today)
-			stock_price.loc[:, 'code'] = stock
-			index = stock_price.index
-			stock_price = stock_price.set_index(['code', index])
-			print(stock_price.index)
-			stock_price.to_sql('stock_price', engine, if_exists = 'append')
-	except AttributeError:
-		print("AttributeError", stock)
-		continue
-	except ValueError:
-		print("ValueError", stock)
+		stock_basics = pd.read_sql('SELECT * from stock_basics', engine)
+		if stock_basics.empty == True:
+			stock_basics = ts.get_stock_basics()
+			stock_concept = ts.get_concept_classified()
+			stock_basics.to_sql('stock_basics', engine, if_exists = 'replace')
+			stock_concept.to_sql('stock_concept', engine, if_exists = 'replace')
+
+		for stock in stock_basics['code']:
+			try:
+				stock_price = ts.get_hist_data(stock, start=today, end=today)
+				stock_price.loc[:, 'code'] = stock
+				index = stock_price.index
+				stock_price = stock_price.set_index(['code', index])
+				print(stock_price.index)
+				stock_price.to_sql('stock_price', engine, if_exists = 'append')
+			except AttributeError:
+				print("AttributeError", stock)
+				continue
+			except ValueError:
+				print("ValueError", stock)
+				continue
+			time.sleep(72000)
+	else:
 		continue
